eb32b422268675b7a22fb978aac65e49804b71ef
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 12:30:39 +0800

    docs: add comprehensive English Javadoc

--- src/main/java/org/apache/maven/spring/boot/MavenClientAutoConfiguration.java
+++ src/main/java/org/apache/maven/spring/boot/MavenClientAutoConfiguration.java
@@ -18,6 +18,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConditionalOnClass({ LocalRepository.class, RepositoryConnectorFactory.class, FileTransporterFactory.class, HttpTransporterFactory.class })
 @EnableConfigurationProperties({ MavenClientProperties.class })
+/**\n * Auto-configuration for Maven client operations.\n *\n * @author [@Loong Wan](https://github.com/loong10k)\n * @since 1.0.0\n */
 public class MavenClientAutoConfiguration {
 	
 	@Bean
