170b217bb98e1c4389c769c8695fc064bd501797
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:40:31 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/maven/spring/boot/utils/ArtifactUtils.java
+++ src/main/java/org/apache/maven/spring/boot/utils/ArtifactUtils.java
@@ -23,7 +23,7 @@ import org.springframework.core.io.FileSystemResource;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 
 public class ArtifactUtils {
